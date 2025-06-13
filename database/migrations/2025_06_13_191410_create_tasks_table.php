1572b3a94acbd123f4f56feddc73e02f0192349d
prosp3ro <70239480+prosp3ro@users.noreply.github.com>
2025-06-13 21:24:31 +0200

    todo app

--- database/migrations/2025_06_13_191410_create_tasks_table.php
+++ database/migrations/2025_06_13_191410_create_tasks_table.php
@@ -11,8 +11,19 @@ return new class extends Migration
      */
     public function up(): void
     {
-        Schema::create('tasks', function (Blueprint $table) {
+        Schema::create("tasks", function (Blueprint $table) {
             $table->id();
+            $table->string("name", length: 255);
+            $table->longText("description")->nullable();
+            // zakladajac, ze priorytet i status sie nie zmienia
+            $table->enum("priority", ["low", "medium", "high"])
+                ->default("medium");
+            $table->enum("status", ["to-do", "in progress", "done"])
+                ->default("to-do");
+            $table->date("due_date");
+            // index - data nie moze byc w przeszlosci
+            // $table->rawIndex(expression: "CHECK (due_date >= CURRENT_DATE)", name: "...");
+            // $table->softDeletes();
             $table->timestamps();
         });
     }
@@ -22,6 +33,6 @@ return new class extends Migration
      */
     public function down(): void
     {
-        Schema::dropIfExists('tasks');
+        Schema::dropIfExists("tasks");
     }
 };
